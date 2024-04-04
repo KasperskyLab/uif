7613d4bc2515a559c966543fd11cf315c0de79f9
Pavel Vostrikov <pavel.vostrikov@kaspersky.com>
2024-04-04 18:48:29 +0300

    feat: change home page for icons

--- packages/kaspersky-icons/.storybook/main.ts
+++ packages/kaspersky-icons/.storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/stories/IconGallery.ts',
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
