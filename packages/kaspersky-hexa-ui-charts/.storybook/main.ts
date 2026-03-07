fb3823ba31c4138b9225dfb6f9bc7465bb692cfb
Pavel Vostrikov <pavel.vostrikov@kaspersky.com>
2026-03-07 21:33:38 +0300

    feat: migrate Charts Storybook build from Webpack to Vite

--- packages/kaspersky-hexa-ui-charts/.storybook/main.ts
+++ packages/kaspersky-hexa-ui-charts/.storybook/main.ts
@@ -1,77 +1,30 @@
-const _ = require('lodash');
-const path = require('path');
-const webpack = require('webpack');
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions'
   ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
-  },
-  webpackFinal: async (config) => {
-    _.set(config, 'resolve.fallback.assert', false);
-    return {
-      ...config,
-      module: {
-        ...config.module,
-        rules: [
-          ...config.module.rules.filter((rule) => !(rule.test && rule.test.test('.svg'))),
-          {
-            test: /\.(less)$/i,
-            use: [
-              'style-loader',
-              'css-loader',
-              {
-                loader: 'less-loader',
-                options: {
-                  javascriptEnabled: true,
-                },
-              },
-            ],
-          },
-          {
-            test: /\.(woff|eot|woff2|ttf)$/,
-            use: 'file-loader',
-          },
-          {
-            test: /\.(scss)$/i,
-            use: [
-              'style-loader',
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: {
-                    auto: true,
-                    localIdentName: '[local]_[hash:base64:5]',
-                  },
-                },
-              },
-              'sass-loader',
-            ],
-          },
-          {
-            test: /\.svg$/,
-            use: {
-              loader: '@svgr/webpack',
-              options: {
-                svgo: false,
-              },
-            },
-          },
-        ],
-      }
+  framework: '@storybook/react-vite',
+  viteFinal: async (config) => {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      assert: false
     }
+    config.css = config.css || {}
+    config.css.preprocessorOptions = {
+      ...config.css.preprocessorOptions,
+      scss: {}
+    }
+    return config
   }
 }
+
 export default config
